6e3a8f6a5e29e4ba9b66fe929c4e44a9d2d8003e
Michael Hellon <kansasboy308@gmail.com>
2024-05-09 20:12:33 -0500

    trying to fix bug
    working on fixing bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // declare Selectors:
 
-const searchCityBtnEl = document.querySelector("#search-button")
+const searchCityBtnEl = document.querySelector("#search-form");
 // declares cityInputE1 variable wnd stores city name once inputted
 const cityInputEl = document.querySelector("#city-Name");
 // declare selector for the clear button
@@ -24,23 +24,28 @@ window.onload = function () {
     }
   };
 
+  console.log('test A-1');
 //
 weatherHistoryEl.addEventListener("click", function (event) {
     // displays weather for any cities in local storage once the cities button is clicked. 
     // if a button says "Denver" it will display Denver's weather when clicked
+    console.log('test A-2');
     if(event.target.matches("button")) {
         let clickedCity = event.target.textContent;
     displayCityForecast(clickedCity);
   }
 });
-
+console.log('test a-3');
 // Clears history when you click "Clear Cities"
 clearBtnEl.addEventListener("click", function() {
+    console.log('test B');
     clearLocalStorage();
     cityUpdateButton();
     clearDisplayedForecast();
-} )
+}
 
+)
+console.log('test B-1')
 
 //add event listener for searchCityBtnEl
 searchCityBtnEl.addEventListener("click", function (event) {
@@ -48,7 +53,7 @@ searchCityBtnEl.addEventListener("click", function (event) {
     event.preventDefault();
     // defines a variable to store the city name inputed
     let searchCityInput = "";
-    
+    console.log('test C');
     if (event.target.matches("button")) {
         searchCityInput = cityInputEl.value.trim()
 
@@ -59,11 +64,12 @@ searchCityBtnEl.addEventListener("click", function (event) {
        } 
     }
 })
+console.log('test B-2')
 
-// clears local storage... 
-function clearLocalStorage() {
-    localStorage.clear();
-}
+// // clears local storage... 
+// function clearLocalStorage() {
+//     // localStorage.clear();
+// }
 
 // clears forecast when there are no cities to display
 function clearDisplayedForecast(){
@@ -71,16 +77,20 @@ function clearDisplayedForecast(){
     forecastEl.innerHTML = "";
     weatherHistoryEl.innerHTML = "";
     todaysWeatherEl.innerHTML = "";
+    console.log('test D')
 }
 
 // clear the search field once click submit
 function clearCityInput() {
     cityInputEl.value = "";
+    console.log('test E')
 }
 
 // shows clear button when cities are displayed
 function clearCityBtn() {
     document.getElementById("clear-button").style.display = "";
+    console.log('test E')
+
 }
 
 // creates the city buttons 
@@ -88,18 +98,23 @@ function cityUpdateButton() {
     // sets the element as an empty string
     weatherHistoryEl.innerHTML = "";
     // creates the city buttons from local storage
+    console.log('test F-1')
+
     for (let i = 0; i < localStorage.length; i++) {
         let city = localStorage.key(i);
         // new button for each city entered with  class='city-button', attribute id='button-
         let newCityEl = $('<button>').addClass('city-button').attr('id', `button-${city}`);
         weatherHistoryEl.append(newCityEl);
+        console.log('test F-2')
+
     }
 } 
 
 // This function provides the lat & lon for the city
 function cityLatAndLon(userCity) {
  
-    const citySearch = `https://api.openweathermap.org/geo/1.0/direct?q=${userCity}&limit=5&appid=${APIKey}`
+    const citySearch = `https://api.openweathermap.org/geo/1.0/direct?q=${userCity}&limit=5&appid=${APIkey}`
+    console.log('test G')
 
     fetch(citySearch)
     .then((response) => response.json())
@@ -121,6 +136,8 @@ function cityLatAndLon(userCity) {
        }
 
 function displayCityForecast(clickedCity) {
+    console.log('test H-1')
+
     // clears city name display
     todaysWeatherEl.innerHTML = "";
     //retrieves the values from local storage
@@ -130,21 +147,31 @@ function displayCityForecast(clickedCity) {
 
     getCityCurrentWeather(latCity, lonCity);
     getFiveDayForecast(latCity, lonCity);
+    console.log('test H-2')
+
 }
 // pulls the parameters gridLat and gridLon in the function and gives you a 5 day forecast
 // the &units=imperial at the end returns imperial measurements (deg F & MPH )
 function getFiveDayForecast(lat, lon) {
+    console.log('test I-1')
+
     let cityForecastSearch = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIkey}&units=imperial`;
     fetch(cityForecastSearch)
         .then((response) => response.JSON())
+        console.log('test I-2')
+
         .then((returnForecast) => {
             displayFiveDayForecast(returnForecast);
+            console.log('test I-3')
+
         });
 }
 
 // pulls the parameters gridLat and gridLon in the function
 // the &units=imperial at the end returns imperial measurements (deg F & MPH )
 function getCityCurrentWeather(lat, lon) {
+    console.log('test J-1')
+
     let cityWeatherSearch = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIkey}&units=imperial`
     fetch(cityWeatherSearch)
     .then((response) => response.json())
@@ -166,6 +193,8 @@ function displayCurrentForecast(currentWeather) {
     let currentWind = currentWeather.wind.speed;
     let currentHumidity = currentWeather.main.humidity;
     let backgroundImageURL = `http://openweathermap.org/img/wn/${currentIcon}@2x.png`;
+    console.log('test K-1')
+
 
     // creates new elements with attributes
     let newDivEl = $('<div>').addClass('card').attr('current-city', current-city.id).css({width: '75%','background-image': `url(${backgroundImageURL})`}).html(`<h1 id="displayedCity">${displayedCity}</h1>`);
@@ -175,6 +204,7 @@ function displayCurrentForecast(currentWeather) {
     newDivEl.append.apply(newParaEl);
     todaysWeatherEl.append(newDivEl)
 
+    console.log('test Final');
 }
 
 /* Acceptance Criteria 
@@ -182,60 +212,60 @@ WHEN I view future weather conditions for that city
 THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 */
 
-// // declared an empty array to store the 5 day forecast in 
-// let fiveDayForecast = [];
+// declared an empty array to store the 5 day forecast in 
+let fiveDayForecast = [];
 
-// function displayFiveDayForecast(forecastWeather) {
-//     // clears five day forecast
-//     forecastEl.innerHTML = ""
-//     // create five day index 
-//     let fiveDayIndex = 0;
-//     // time offset, change (- or + hrs) to adjust time relative to noon
-//     let timeOffset = 0;
-//     // finds the time zone of the current city
-//     let forecastedTimezone = forecastWeather.city.timezone;
-//     let forecastedTime = forecastWeather.list.[fiveDayIndex].dt;
-//     let forecastedAdjTime = forecastWeather + forecastedTimezone;
-//     // formats the forecasted Hour to 24hr clock to just the hour element to use in the next step 
-//     forecastedHour = moment.unix(forecastedAdjTime).format("HH");
-//     // calls the roundTimeOff function 
-//     forecastedHour = roundTimeOff(forecastedHour);
+function displayFiveDayForecast(forecastWeather) {
+    // clears five day forecast
+    forecastEl.innerHTML = ""
+    // create five day index 
+    let fiveDayIndex = 0;
+    // time offset, change (- or + hrs) to adjust time relative to noon
+    let timeOffset = 0;
+    // finds the time zone of the current city
+    let forecastedTimezone = forecastWeather.city.timezone;
+    let forecastedTime = forecastWeather.list.fiveDayIndex.dt;
+    let forecastedAdjTime = forecastWeather + forecastedTimezone;
+    // formats the forecasted Hour to 24hr clock to just the hour element to use in the next step 
+    forecastedHour = moment.unix(forecastedAdjTime).format("HH");
+    // calls the roundTimeOff function 
+    forecastedHour = roundTimeOff(forecastedHour);
 
-//     // as long as forecastedHour does not equal to 12 (noon) and timeOffset is less that 8
-//     while (forecastedHour != 12 && timeOffset < 8) {
-//         timeOffset++ // time gets incremented
-//         forecastedTime = forecastWeather.list.[fiveDayIndex].dt
-//         forecastedAdjTime = forecastWeather + forecastedTimezone;
-//         // formats the forecasted Hour to 2 digits (includes the 0 prior to 1000 hrs) to use in the next step 
-//         forecastedHour = moment.unix(forecastedAdjTime).format("KK");
-//         forecastedHour = roundTimeOff(forecastedHour);
-//     }
-//     // loop thru forecastWeather and add 8hrs
-//     for (let index = timeOffset; index < forecastWeather.list.length; index+8 ) {
-//         fiveDayForecast[fiveDayIndex] = forecastWeather.list.[timeOffset].dt
+    // as long as forecastedHour does not equal to 12 (noon) and timeOffset is less that 8
+    while (forecastedHour != 12 && timeOffset < 8) {
+        timeOffset++ // time gets incremented
+        forecastedTime = forecastWeather.list.fiveDayIndex.dt
+        forecastedAdjTime = forecastWeather + forecastedTimezone;
+        // formats the forecasted Hour to 2 digits (includes the 0 prior to 1000 hrs) to use in the next step 
+        forecastedHour = moment.unix(forecastedAdjTime).format("KK");
+        forecastedHour = roundTimeOff(forecastedHour);
+    }
+    // loop thru forecastWeather and add 8hrs
+    for (let index = timeOffset; index < forecastWeather.list.length; index+8 ) {
+        fiveDayForecast[fiveDayIndex] = forecastWeather.list.timeOffset.dt
 
-//         let forecastedDate = moment.unix(forecastWeather.list[index].dt).format("DD MMM YYYY"); 
-//         let forecastedTemp = fiveDayForecast[fiveDayIndex].main.temp.toFixed(2);
-//         let forecastedWind = fiveDayForecast[fiveDayIndex].wind.speed;
-//         let forecastedHumidity = fiveDayForecast[fiveDayIndex].main.humidity;
-//         let forecastedIcon = fiveDayForecast[fiveDayIndex].weather[0].icon
+        let forecastedDate = moment.unix(forecastWeather.list[index].dt).format("DD MMM YYYY"); 
+        let forecastedTemp = fiveDayForecast[fiveDayIndex].main.temp.toFixed(2);
+        let forecastedWind = fiveDayForecast[fiveDayIndex].wind.speed;
+        let forecastedHumidity = fiveDayForecast[fiveDayIndex].main.humidity;
+        let forecastedIcon = fiveDayForecast[fiveDayIndex].weather[0].icon
 
-//     // creates new elements with attributes
-//         let newDivEl = $('<div>').addClass('card').attr(' 'day' + (fiveDayIndex + 1)', day.id).css({width: '15%','background-image': `url(${backgroundImageURL})`}).html(`<h3 class="date-header">${forecastedDate} </h3>`);
-//         let newImgEl = $('<img>').addClass('card-img-top').attr('src = `http://openweathermap.org/img/wn/${forecastedIcon}@2x.png` ','alt = 'Weather Icon'');
-//         let newDivTwoEl =$('<div>').addClass('card-body')
-//         let newParaEl = $("<p>").addClass('card-text').html(`<div>Temp: ${forecastedTemp} °F </div> <div>Wind: ${forecastedWind} MPH</div> <div>Humidity: ${forecastedHumidity} %</div>`); 
+    // creates new elements with attributes
+        let newDivEl = $('<div>').addClass('card').attr(`day + (fiveDayIndex + 1)`, day.id).css({width: '15%','background-image': `url(${backgroundImageURL})`}).html(`<h3 class="date-header">${forecastedDate} </h3>`);
+        let newImgEl = $('<img>').addClass('card-img-top').attr('src = `http://openweathermap.org/img/wn/${forecastedIcon}@2x.png` ','alt = Weather Icon');
+        let newDivTwoEl =$('<div>').addClass('card-body')
+        let newParaEl = $("<p>").addClass('card-text').html(`<div>Temp: ${forecastedTemp} °F </div> <div>Wind: ${forecastedWind} MPH</div> <div>Humidity: ${forecastedHumidity} %</div>`); 
 
-//         // finally append everything...
-//         newDivEl.append(newImgEl);
-//         newDivTwoEl.append(newParaEl);
-//         newDivEl.append(newDivTwoEl);
+        // finally append everything...
+        newDivEl.append(newImgEl);
+        newDivTwoEl.append(newParaEl);
+        newDivEl.append(newDivTwoEl);
 
-//         forecastEl.append(newDivEl);
+        forecastEl.append(newDivEl);
 
-//         fiveDayIndex++
-//         }
-// }
+        fiveDayIndex++
+        }
+}
 
 // rounds time off so that you can get 5 day forecast that changes through-out the day. 
 function roundTimeOff(time) {
